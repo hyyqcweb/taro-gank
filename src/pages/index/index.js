256c93c39ddef40b289a669c95a06f1d6e164560
hyyqc <junjie_hyyqc@163.com>
2018-12-28 13:34:01 +0800

    :fire::sparkles: fix index

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,7 +1,8 @@
 import Taro, {Component} from '@tarojs/taro'
-import {View, Swiper, SwiperItem, ScrollView} from '@tarojs/components'
+import {View, Swiper, SwiperItem, ScrollView, Text} from '@tarojs/components'
 import './index.less'
 
+let page = 1;
 export default class Index extends Component {
 
   config = {
@@ -14,6 +15,7 @@ export default class Index extends Component {
       loading: true,
       navTab: ['Android', 'iOS', '前端'],
       currentIndex: 0,
+      List: []
     }
   }
 
@@ -22,78 +24,123 @@ export default class Index extends Component {
     this.getData(navTab[0]);
   }
 
-  getData(value) {
+  getData(value, index) {
+    Taro.showLoading({ title: '加载中' });
     Taro.request({
-      url: `http://gank.io/api/data/${value}/10/1`,
+      url: `http://gank.io/api/data/${value}/10/${page}`,
       header: {
         'Content-Type': 'application/json'
       }
     }).then(res => {
-      console.log(res);
+      const { data } = res;
+      Taro.hideLoading();
+      if(index === undefined) {
+        this.setState({
+          List: data.results,
+          loading: false
+        })
+      }else {
+        const { List } = this.state;
+        this.setState({
+          List: List.concat(data.results),
+          loading: false
+        })
+      }
     }).catch(err => {
       console.log(err);
     })
   }
 
-
-  onClick = (id, index) => {
+  onClick = (index) => {
     this.setState({
       currentIndex: index
     })
   };
 
-  handleLeft = (e) => {
+  handleToTop = (e) => {
     console.log(e);
   };
 
-  handleRight = (e) => {
-    console.log(e)
+  handleToBottom = () => {
+    const { currentIndex, navTab } = this.state;
+    page++;
+    this.getData(navTab[currentIndex], page);
   };
 
   handleChange = (e) => {
+    const { navTab } = this.state;
     this.setState({
       currentIndex: e.detail.current
     });
+    console.log(e);
+    this.getData(navTab[e.detail.current]);
+  };
+
+  handleDetail = (item) => {
+    Taro.navigateTo({
+      url: `/pages/detail/index?item=${JSON.stringify(item)}`
+    })
   };
 
   render() {
-    const { navTab, currentIndex } = this.state;
+    const { navTab, currentIndex, List, loading } = this.state;
+    console.log('List', List);
     return (
       <View className='index'>
-        <ScrollView
-          scrollX
-          scrollWithAnimation
-          onScrollToUpper={this.handleLeft}
-          onScrollToLower={this.handleRight}
-          lowerThreshold='20'
-          upperThreshold='20'
-        >
-          <View className='title'>
-            {navTab.map((d, i) =>
-              <View className={currentIndex === i ? 'flex-item active' : 'flex-item'} key={d.id} onClick={this.onClick.bind(this, d.id, i)}>
-                {d}
-              </View>
-            )}
-          </View>
-        </ScrollView>
-        <Swiper
-          className='content'
-          vertical={false}
-          circular
-          indicatorDots={false}
-          current={currentIndex}
-          onChange={this.handleChange.bind(this)}
-        >
-          {
-            navTab.map((d, i) => (
-              <SwiperItem key={i} className='nav-item'>
-                <View className={'demo-text-' + i}>
-                  {i}
+        <View className='title'>
+          {navTab.map((d, i) =>
+            <View className={currentIndex === i ? 'flex-item active' : 'flex-item'} key={d.id} onClick={this.onClick.bind(this, i)}>
+              {d}
+            </View>
+          )}
+        </View>
+        <View className='content'>
+          <Swiper
+            className='test-h'
+            vertical={false}
+            circular
+            indicatorDots={false}
+            current={currentIndex}
+            onChange={this.handleChange.bind(this)}
+          >
+            {navTab.map((d, i) => (
+              <SwiperItem key={i}>
+                <View className={`demo-text-${i}`}>
+                  <ScrollView
+                    className='scroll-view'
+                    scrollY
+                    scrollWithAnimation
+                    scrollTop='0'
+                    enableBackToTop
+                    lowerThreshold='20'
+                    upperThreshold='20'
+                    onScrollToUpper={this.handleToTop.bind(this)}
+                    onScrollToLower={this.handleToBottom.bind(this)}
+                  >
+                    {!loading && List.map((t, r) => {
+                      return <View className='item-list' key={r} onClick={this.handleDetail.bind(this, t)}>
+                        <View className='item-content'>
+                          {t.desc}
+                        </View>
+                        <View className='item-bottom'>
+                          <View className='fl'>
+                            {t.publishedAt.substring(0, 10)}
+                          </View>
+                          <View className='fr'>
+                            {t.who}
+                          </View>
+                        </View>
+                      </View>
+                    })}
+                    <View className='load-more-container'>
+                      <Text className='load-more-tips'>加载更多数据...</Text>
+                    </View>
+                  </ScrollView>
                 </View>
               </SwiperItem>
-            ))
-          }
-        </Swiper>
+            ))}
+          </Swiper>
+        </View>
       </View>
     )
   }
